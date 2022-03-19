9d6aa8499f99451711c08330fc5f05ec8b2eac12
Ali Efe <aliefekaragul@gmail.com>
2022-03-20 00:28:40 +0300

    basic functions added

--- main.c
+++ main.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 
-int main(){
+void printMemoryD(int * ptr, int * endptr){
+    int i;
+    for (i = 0; ptr + i != endptr; i++) 
+        printf("%d", ptr[i]);
+}
 
+void applyCommand(char * command, int * ptr, int * endptr){
+    int i;
+    char c;
+    for (i = 0; command[i] != '\0' && ptr != NULL; i++){
+        c = command[i];
+
+        if (c == '>') {
+            ptr++;
+            endptr++;
+        }
+        if (c == '<') ptr--;
+        if (c == '+') (*ptr)++;
+        if (c == '-') (*ptr)--;
+    }
+}
+
+int main(){ 
+    int memory[1000] = {0}, *ptr = &memory[0], *endptr = &memory[0];
+    char command[100];
+    
+
+    
 
     return 0;
 }
